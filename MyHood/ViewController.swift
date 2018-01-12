1dc69b6320338c6f307c36aca699c22ae8a858b8
김주현 <inyeobkim@inyeobui-macbook-pro.local>
2018-01-11 21:52:06 -0500

    Initial Commit

---
+++ MyHood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHood
+//
+//  Created by Inyeob Kim on 1/11/18.
+//  Copyright © 2018 Inyeob Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
